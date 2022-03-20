af543113ab1bc8417c0e2763d137e2afb79f940b
Lexie-DelViscio <delvisciolp@g.cofc.edu>
2022-03-20 14:27:39 -0400

    added ggboxplots and some simple models

--- startisotope.R
+++ startisotope.R
@@ -30,16 +30,35 @@ mean_site_pico_percent = tapply(isotope_data$atom_15N_percent,isotope_data$Panel
 barplot(mean_site_pico,las=2)
 barplot(mean_site_pico_percent, las=2)
 
+library(ggplot2)
+ggplot(data = isotope_data) + 
+  geom_boxplot(mapping = aes(x = Taxa, y = pedino_13C_enrichment)) +  
+  labs(x = 'Taxa', y = 'pedino_13C_enrichment', title = 'taxa vs pedino_13C_enrichment') 
+ggplot(data = isotope_data) + 
+  geom_boxplot(mapping = aes(x = Taxa, y = pico_15N_enrichment)) +  
+  labs(x = 'Taxa', y = 'pico_15N_enrichment', title = 'taxa vs pico_15N_enrichment') 
+
 pedinotaxa = lm(isotope_data$pedino_13C_enrichment ~ isotope_data$Taxa)
 pedinosite = lm(isotope_data$pedino_13C_enrichment ~ isotope_data$Panel_Site)
 
 unique(isotope_data$Panel_Site)
 summary(pedinosite)
+summary(pedinotaxa)
 
 pedinospecies = lm(isotope_data$pedino_13C_enrichment ~ isotope_data$Species_ID)
-
-
 summary(pedinospecies)
+
+
+picotaxa = lm(isotope_data$pico_15N_enrichment ~ isotope_data$Taxa)
+picosite = lm(isotope_data$pico_15N_enrichment ~ isotope_data$Panel_Site)
+
+summary(picotaxa)
+summary(picosite)
+
+
+
+#str(dune_mds)
+
 # pico_cytometry <- cytometry_file[ -c(0,14:25) ]
 # pico_cytometry
 # pedino_cytometry <- cytometry_file[ -c(0,2:13) ]
